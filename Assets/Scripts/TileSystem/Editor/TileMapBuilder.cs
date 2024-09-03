8de6e461ac990d5895627004c76a5a23e49efd8b
Marissa Moser <marissalmoser@gmail.com>
2024-09-03 14:46:14 -0500

    empty boarder

--- Assets/Scripts/TileSystem/Editor/TileMapBuilder.cs
+++ Assets/Scripts/TileSystem/Editor/TileMapBuilder.cs
@@ -5,16 +5,16 @@
 *    Description: A custom editor window to build a map of tiles.
 *******************************************************************/
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using UnityEditor;
 using UnityEngine;
 
 public class TileMapBuilder : EditorWindow
 {
     Vector2Int _mapSize = new Vector2Int(3,3);
+    int _emptyBorderSize = 3;
     GameObject _defaultTilePrefab;
     GameObject _tileParentObj;
-
-    private List<GameObject> _currentMap = new List<GameObject> { };
 
     /// <summary>
     /// Function that allows the Tile Map Builder to have its own window. Access it
@@ -33,6 +33,8 @@ public class TileMapBuilder : EditorWindow
     {
         //serialized fields
         _mapSize = EditorGUILayout.Vector2IntField("Map Size", _mapSize);
+
+        _emptyBorderSize = EditorGUILayout.IntField("Empty Boarder Size", _emptyBorderSize);
 
         _defaultTilePrefab = EditorGUILayout.ObjectField("Default Tile Prefab",
             _defaultTilePrefab, typeof(GameObject), false) as GameObject;
@@ -55,22 +57,30 @@ public class TileMapBuilder : EditorWindow
     }
 
     /// <summary>
-    /// Function to create a new map of tiles based on _mapSize
+    /// Function to create a new map of tiles based on _mapSize with a border of
+    /// empty "hole" tiles around it.
     /// </summary>
     private void CreateNewTileMap()
     {
         ClearTileMap();
 
-        for (int i = 0; i < _mapSize.x; i++)
+        for (int i = -_emptyBorderSize; i < _mapSize.x + _emptyBorderSize; i++)
         {
-            for (int j = 0; j < _mapSize.y; j++)
+            for (int j = -_emptyBorderSize; j < _mapSize.y + _emptyBorderSize; j++)
             {
-                //Create and set up tiles for map
-                GameObject go = Instantiate(_defaultTilePrefab, new Vector3(i, 0, j), Quaternion.identity, _tileParentObj.transform);
-                _currentMap.Add(go);
+                //Create tiles for map
+                GameObject go = Instantiate(_defaultTilePrefab, new Vector3(i, 0, j),
+                    Quaternion.identity, _tileParentObj.transform);
 
                 //Set up tile's fields
-                go.GetComponent<Tile>().SetCoordinates(new Vector2(i,j));
+                Tile tile = go.GetComponent<Tile>();
+                tile.SetCoordinates(new Vector2(i, j));
+
+                //makes hole tiles around the map
+                if (i < 0 || i >= _mapSize.x || j < 0 || j >= _mapSize.y)
+                {
+                    tile.SetTileType(Tile.TileType.Hole);
+                }
             }
         }
     }
@@ -80,9 +90,22 @@ public class TileMapBuilder : EditorWindow
     /// </summary>
     private void ClearTileMap()
     {
-        foreach (GameObject go in _currentMap)
+        //ensures the tile parent is not null
+        if(_tileParentObj == null)
         {
-            DestroyImmediate(go);
+            Debug.LogError("Tile Parent field is null; Drag a game object into the inspector");
+            return;
+        }
+
+        Transform[] tiles = _tileParentObj.GetComponent<Transform>()
+            .GetComponentsInChildren<Transform>(true);
+
+        for(int i = tiles.Length - 1; i > 0; i--)
+        {
+            if(tiles[i].gameObject != _tileParentObj)
+            {
+                DestroyImmediate(tiles[i].gameObject);
+            }
         }
     }
 }
--- Assets/Scripts/TileSystem/Tile.cs
+++ Assets/Scripts/TileSystem/Tile.cs
@@ -186,5 +186,4 @@ public class Tile : MonoBehaviour
 
     #endregion
 
-
 }
