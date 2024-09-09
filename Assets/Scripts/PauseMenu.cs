33dfbadc59f85d91d42a1684706bd7bed7d82f5b
Veizyl <sturner@mail.bradley.edu>
2024-09-09 18:08:20 -0500

    Push

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private PlayerInput _playerInput;
+    [SerializeField] private GameObject _pauseMenu;
+    private bool _isPaused = false;
+
+    private void Start()
+    {
+        // Subscribe to input events
+        _playerInput.currentActionMap["Pause"].performed += ctx =>
+            Pause();
+    }
+    
+    private void Pause()
+    {
+        _isPaused = !_isPaused;
+
+        if (_isPaused)
+        {
+            _pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            _pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+    
+}
