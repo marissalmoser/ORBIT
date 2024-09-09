7e1d33300c6c8d64299f77d75bf35257881ce172
Veizyl <sturner@mail.bradley.edu>
2024-09-09 13:31:26 -0500

    Commenting Code

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -1,3 +1,10 @@
+/******************************************************************
+*    Author: Sky Turner
+*    Contributors: 
+*    Date Created: 9/5/24
+*    Description: This script loads a scene for level selection
+*******************************************************************/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +12,10 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
+    /// <summary>
+    /// This function loads a scene based on the int passed through
+    /// </summary>
+    /// <param name="levelNumber">The build index for the scene</param>
    public void LoadLevel(int levelNumber)
     {
         SceneManager.LoadScene(levelNumber);
