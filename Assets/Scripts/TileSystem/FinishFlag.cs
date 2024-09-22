24eb7b2da637415d4b44c18da24c8ac3302982fb
evroman03 <evroman@mail.bradley.edu>
2024-09-22 13:12:46 -0500

    FinishFix

--- Assets/Scripts/TileSystem/FinishFlag.cs
+++ Assets/Scripts/TileSystem/FinishFlag.cs
@@ -15,12 +15,21 @@ public class FinishFlag : Collectable
             _PSMB = other.GetComponent<PlayerStateMachineBrain>();
             StartCoroutine(WaitCoroutine());
         }
+        else if (other.CompareTag("PlayerGhost"))
+        {
+            PlayerStateMachineBrain psmb = other.gameObject.GetComponentInParent<PlayerStateMachineBrain>(true);
+            while (psmb.GetNextAction() != null) //removes all remaining actions
+            {
+
+            }
+            PlayerController.ReachedDestination?.Invoke();
+        }
     }
     private IEnumerator WaitCoroutine()
     {
         //_PSMB.SetCardList(null);
         _PSMB.StartCardActions(null);
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(.75f);
         GameManager.Instance.ChangeGameState(GameManager.STATE.End);
         print("WIN");
     }
