3b2f6a41bdcff25389d5cb7964ef7e43d370fea8
Marissa Moser <marissalmoser@gmail.com>
2024-10-10 19:59:02 -0500

    code header

--- Assets/Scripts/UI/OptionsMenuFunctionality.cs
+++ Assets/Scripts/UI/OptionsMenuFunctionality.cs
@@ -1,5 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+/******************************************************************
+ *    Author: Marissa 
+ *    Contributors: 
+ *    Date Created: 10/10/24
+ *    Description: Contains functionality for the options menu.
+ *******************************************************************/
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,12 +12,18 @@ public class OptionsMenuFunctionality : MonoBehaviour
     [SerializeField] private Slider _sfxSlider;
     [SerializeField] private Slider _musicSlider;
 
+    /// <summary>
+    /// Sets the sliders on the options menu to match the current volume.
+    /// </summary>
     void Start()
     {
         _sfxSlider.value = SfxManager.Instance.GetCurrentVolume();
         _musicSlider.value = MusicManager.Instance.GetCurrentVolume();
     }
 
+    /// <summary>
+    /// Activates the button SFX.
+    /// </summary>
     public void ActivateSFX()
     {
         SfxManager.Instance.PlaySFX(6893);
