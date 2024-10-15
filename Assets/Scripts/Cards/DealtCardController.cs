af6e52e61b53ad94910f8f3c39a9fa89f4c3da5b
Marissa Moser <marissalmoser@gmail.com>
2024-10-15 16:52:16 -0500

    Delete Assets/Scripts/Cards/DealtCardController.cs

--- Assets/Scripts/Cards/DealtCardController.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DealtCardController : MonoBehaviour
-{
-    //UIManager.Instance.GetInstantiatedDealtCardImages();
-}
