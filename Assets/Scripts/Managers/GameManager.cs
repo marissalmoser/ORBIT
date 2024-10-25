ac558add5a22522367a83a1baa6eb4245eabfad0
Veizyl <sturner@mail.bradley.edu>
2024-10-25 13:16:54 -0500

    push

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -36,6 +36,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private List<Card> _playedCards;
     [SerializeField] bool _doDebugMode;
     [SerializeField] private int _deathTimerLength;
+    [SerializeField] private Texture2D _clearCursor;
+    [SerializeField] private Texture2D _switchCursor;
+    private Vector2 _clearCursorHotspot;
+    private Vector2 _switchCursorHotspot;
+    private Vector2 _sunnnyCursorHotspot = new Vector2(16, 16);
 
     public Image darken;
     public Image deckShownDarken;
@@ -96,6 +101,14 @@ public class GameManager : MonoBehaviour
         hasSwitched = false;
         _getOriginalDeck = true;
 
+        if (_clearCursor != null)
+        {
+            _clearCursorHotspot = new Vector2(_clearCursor.width / 2, _clearCursor.height / 2);
+        }
+        if (_switchCursor != null)
+        {
+            _switchCursorHotspot = new Vector2(_switchCursor.width / 2, _switchCursor.height / 2);
+        }
         ChangeGameState(STATE.LoadGame);
     }
 
@@ -143,6 +156,7 @@ public class GameManager : MonoBehaviour
             case STATE.ChooseCards:
                 //Choose Cards methods called
                 gameState = STATE.ChooseCards;
+                Cursor.SetCursor(null, _sunnnyCursorHotspot, CursorMode.Auto);
                 DealCards();
                 break;
             case STATE.ConfirmCards:
@@ -151,6 +165,7 @@ public class GameManager : MonoBehaviour
                 break;
             case STATE.PlayingActionOrder:
                 gameState = STATE.PlayingActionOrder;
+                Cursor.SetCursor(null, _sunnnyCursorHotspot, CursorMode.Auto);
                 break;
             case STATE.ChooseTurn:
                 // Waiting STATE. Game locks in this state until user input
@@ -276,6 +291,11 @@ public class GameManager : MonoBehaviour
                 darken.enabled = true;
                 isClearing = true;
                 _uiManager.UpdateTextBox("SELECT A CARD TO CLEAR.");
+
+                if (_clearCursor != null)
+                { 
+                    Cursor.SetCursor(_clearCursor, _clearCursorHotspot, CursorMode.Auto);
+                } 
             }
             else
             {
@@ -292,7 +312,13 @@ public class GameManager : MonoBehaviour
                 darken.enabled = true;
                 isSwitching = true;
                 _uiManager.UpdateTextBox("SELECT TWO CARDS TO SWAP.");
+
+                if (_switchCursor != null)
+                { 
+                    Cursor.SetCursor(_switchCursor, _switchCursorHotspot, CursorMode.Auto);
+                } 
             }
+
             else
             {
                 confirmationCard = null;
