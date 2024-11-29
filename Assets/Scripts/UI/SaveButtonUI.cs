99e7c8fcc563b55a1e253f06b1958e36f87ff0c2
EeveeonGamer27 <101752853+EeveeonGamer27@users.noreply.github.com>
2024-11-29 03:12:17 -0600

    Ramp Added and File Select

--- Assets/Scripts/UI/SaveButtonUI.cs
+++ Assets/Scripts/UI/SaveButtonUI.cs
@@ -7,13 +7,32 @@
 *******************************************************************/
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveButtonUI : MonoBehaviour
 {
-    [SerializeField] private TMP_Text text;
+    [SerializeField] private Image image;
+    [SerializeField] private Sprite Icon1;
+    [SerializeField] private Sprite Icon2;
+    [SerializeField] private Sprite Icon3;
 
-    public void Start()
+    public void OnEnable()
     {
-        text.text = SaveLoadManager.Instance.GetCurrentSaveSelected().ToString();
+        Invoke("ChangeSaveImage", 0.5f);
+    }
+    public void ChangeSaveImage()
+    {
+        if (SaveLoadManager.Instance.GetCurrentSaveSelected().ToString() == "1")
+        {
+            image.sprite = Icon1;
+        }
+        else if (SaveLoadManager.Instance.GetCurrentSaveSelected().ToString() == "2")
+        {
+            image.sprite = Icon2;
+        }
+        else if (SaveLoadManager.Instance.GetCurrentSaveSelected().ToString() == "3")
+        {
+            image.sprite = Icon3;
+        }
     }
 }
