6edcf41900c1d9be1ea09d017b90c47461b96d4e
PeachieDotZip <101476073+PeachieDotZip@users.noreply.github.com>
2025-01-29 00:09:17 -0600

    Moon Particle Initial Implementation
    -Added particle for moon collectable. Moon organization on file side is going thru kind of a complete rework, as there will now be 5 different collectable prefabs to coincide with the differently-colored moons. This is so each moon can have their own particle color.
    -Moon particles always face the camera and are drawn behind moon object, accomplished thru use of a new script called "LookAt" which tells a parent of the particle system to always face either a target OR the main camera in a scene.
    -Added "GeneralUse" subfolder to Scripts folder, and placed the LookAt script in that folder.
    -Added "Collectables" subfolder to Prefabs folder, and placed the World 1 collectable prefab in that folder. "Collectable_W1" is the only prefab completed as of rn, and not every level has been updated with new prefab. Collectable particle total reorganization and particle implementation is forthcoming.
    -i eated peanut butterrrrr yumm

---
+++ Assets/Scripts/GeneralUse/LookAt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform target;
+    [SerializeField] private bool lookAtCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (lookAtCam == true)
+        {
+            target = Camera.main.transform;
+        }
+        transform.LookAt(target, Vector3.up);
+    }
+    void Update()
+    {
+        // Rotate the camera every frame so it keeps looking at the target
+        //transform.LookAt(target);
+
+        // Same as above, but setting the worldUp parameter to Vector3.up
+        transform.LookAt(target, Vector3.up);
+    }
+
+}
